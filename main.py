b3ac3ffca90c97e8402ae41c5dfc8393350215b1
Radek Janeček <83546285+RadekJanecek@users.noreply.github.com>
2022-04-28 11:39:37 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -4,3 +4,6 @@ def on_button_pressed_a():
     basic.show_icon(IconNames.YES)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def on_received_number(receivedNumber):
+    print(String.from_char_code(receivedNumber))
+radio.on_received_number(on_received_number)
--- main.ts
+++ main.ts
@@ -3,3 +3,6 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
     start
     basic.showIcon(IconNames.Yes)
 })
+radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
+    console.log(String.fromCharCode(receivedNumber))
+})
